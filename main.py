45a4f66d0aa4ac22a09b975b4d18756015d61e64
andluiwinck <andluiwinck@hotmail.com>
2025-11-11 12:32:05 -0300

    add code to take a file path of a book and count words on main.py

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("greetings boots")
+def get_book_text(path_to_file):
+    with open(path_to_file) as f:
+        book_text = f.read()
+    return book_text
+
+def count_words(text):
+    words = text.split()
+    return len(words)
+
+def main():
+    
+    book_path = "books/frankenstein.txt"
+    
+    text = get_book_text(book_path)
+    num_words = count_words(text)
+    
+    print(f"Found {num_words} total words")
+
+main()
